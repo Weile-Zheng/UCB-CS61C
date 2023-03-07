30ed8676bf51ae179fe1374057788468cfd6ef26
Weile <wzheng13@u.rochester.edu>
2023-03-06 19:16:20 -0500

    Lab 01 Complete

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -1,7 +1,45 @@
 #include <stddef.h>
+#include <stdio.h>
 #include "ll_cycle.h"
 
-int ll_has_cycle(node *head) {
-    /* your code here */
+int ll_has_cycle(node *head)
+{
+    node *tortoise = head;
+    node *hare = head;
+
+    // Check for empty list
+    if (head == NULL)
+    {
+        return 0;
+    }
+    // If list only has one element
+    if (tortoise->next == NULL)
+    {
+        return 0;
+    }
+
+    // Since above proved next is not null, we can safely move hare down next twice without null pointer error.
+    tortoise = tortoise->next;
+    hare = hare->next->next;
+
+    while (tortoise != NULL && hare != NULL)
+    {
+        if (tortoise == hare)
+        {
+            return 1;
+        }
+
+        tortoise = tortoise->next;
+
+        if (hare->next != NULL)
+        {
+            hare = hare->next->next;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
     return 0;
 }
